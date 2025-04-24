6b762e48096f508b12dd4d00d2a6e5b063407a29
Varun Bhat <varbhat10@gmail.com>
2025-04-24 13:51:55 -0700

    deployment setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // If your GitHub repo name is NOT yourusername.github.io,
+  // uncomment and update the following line:
+  // basePath: '/your-repo-name',
 };
 
 module.exports = nextConfig; 

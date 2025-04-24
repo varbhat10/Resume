6ec9e129233b73222b792de45b1ded1bf4ffa05c
Varun Bhat <varbhat10@gmail.com>
2025-04-24 13:58:31 -0700

    Configure website for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -7,7 +7,9 @@ const nextConfig = {
   },
   // If your GitHub repo name is NOT yourusername.github.io,
   // uncomment and update the following line:
-   basePath: 'varbhat10/Resume',
+  basePath: '/Resume',
+  // Add assetPrefix for GitHub Pages
+  assetPrefix: '/Resume',
 };
 
 module.exports = nextConfig; 
